cb480a9956ecbb5571e90891680a0163b1dd1977
jmleong666 <ljming4848@hotmail.com>
2021-02-21 23:42:57 +0800

    Set default folders and add input validation

--- app.py
+++ app.py
@@ -2,25 +2,41 @@ import PySimpleGUI as sg
 import os.path
 import traceback
 
-inputPath = ""
-outputFileName = ""
-outputFolder = ""
+DEFAULT_INPUT_FOLDER = "videos"
+DEFAULT_OUTPUT_FOLDER = "output"
+input_path = DEFAULT_INPUT_FOLDER
+output_folder = DEFAULT_OUTPUT_FOLDER
+output_path_name = ""
+file_selected = False
+
+try:
+    # Get list of files in default folder
+    file_list = os.listdir(DEFAULT_INPUT_FOLDER)
+except:
+    file_list = []
+
+default_fnames = [
+    f
+    for f in file_list
+    if os.path.isfile(os.path.join(DEFAULT_INPUT_FOLDER, f))
+    and f.lower().endswith((".png", ".gif", ".mp4"))
+]
 
 # window layout
 file_list_column = [
     [
         sg.Text("Source Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-SRC FOLDER-"),
+        sg.In(default_text=DEFAULT_INPUT_FOLDER, size=(25, 1), enable_events=True, key="-SRC FOLDER-"),
         sg.FolderBrowse(),
     ],
     [
         sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+            values=default_fnames, enable_events=True, size=(40, 20), key="-FILE LIST-"
         )
     ],
     [
         sg.Text("Output Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-OUT FOLDER-"),
+        sg.In(default_text=DEFAULT_OUTPUT_FOLDER, size=(25, 1), enable_events=True, key="-OUT FOLDER-"),
         sg.FolderBrowse(),
     ],
     [
@@ -64,31 +80,35 @@ while True:
             and f.lower().endswith((".png", ".gif", ".mp4"))
         ]
         window["-FILE LIST-"].update(fnames)
+        file_selected = False
     elif event == "-OUT FOLDER-":
-        outputFolder = values["-OUT FOLDER-"]
+        output_folder = values["-OUT FOLDER-"]
     elif event == "-FILE LIST-":  # A file was chosen from the listbox
         try:
             filename = os.path.join(
                 values["-SRC FOLDER-"], values["-FILE LIST-"][0]
             )
-            inputPath = filename
+            input_path = filename
             window["-TOUT-"].update(filename)
             basename = os.path.basename(filename)
-            outputFileName = os.path.splitext(basename)[0]
+            output_path_name = os.path.splitext(basename)[0]
+            file_selected = True
 
         except:
             pass
     elif event == "-RUN COUNTER-":
-        cliInputStrings = ("python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input ", 
-                          " --output ", ".avi --output-csv ", ".csv --output-plots ")
-        outputPath = os.path.join(outputFolder, outputFileName)
-        finalInput = cliInputStrings[0] + inputPath + cliInputStrings[1] + outputPath + cliInputStrings[2] + outputPath + cliInputStrings[3] + outputPath
-        print(finalInput)
-        try:
-            os.system(finalInput) # insert run script here
-        except Exception as e:
-            tb = traceback.format_exc()
-            sg.Print(f'An error happened.  Here is the info:', e, tb)
-        sg.Popup("Success!")
+        if file_selected:
+            if output_folder == DEFAULT_OUTPUT_FOLDER:
+                if not os.path.exists(DEFAULT_OUTPUT_FOLDER):
+                    os.mkdir(DEFAULT_OUTPUT_FOLDER)
+            cli_input_strings = ("python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input ", 
+                            " --output ", ".avi --output-csv ", ".csv --output-plots ")
+            output_path = os.path.join(output_folder, output_path_name)
+            final_input = cli_input_strings[0] + input_path + cli_input_strings[1] + output_path + cli_input_strings[2] + output_path + cli_input_strings[3] + output_path
+            print(final_input)
+            os.system(final_input) # insert run script here
+            sg.Popup("Success!")
+        else:
+            sg.popup_error(f'ERROR', "Please select a file!")
 
 window.close()
